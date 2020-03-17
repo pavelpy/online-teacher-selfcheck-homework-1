4b58f5d325bdbdd0ab8a2f38678f32b60a9a6ca6
Pavel K <pavel.py3@gmail.com>
2020-03-17 10:55:35 +0300

    imp: add green color for point.

--- renderView.js
+++ renderView.js
@@ -157,7 +157,7 @@ export function render(criteria) {
       list.map((item, i) => {
         let strNum = item.mod + '';
         let points = getPointAppend(strNum[strNum.length - 1]);
-        content.innerHTML += `<p>${i + 1}) ${item.text}: + ${strNum} ${points} \r\n</p>`;
+        content.innerHTML += `<p>${i + 1}) ${item.text}</p><p style="color:green"> + ${strNum} ${points} \r\n</p>`;
       });
       toClipBoard = content.innerText;
 
